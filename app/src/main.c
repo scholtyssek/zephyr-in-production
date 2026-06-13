638f7918852ec5c82da9c85ffb180225ed4e10e0
Marco Scholtyssek <9295322+scholtyssek@users.noreply.github.com>
2026-06-13 19:43:19 +0200

    feat: block4

--- app/src/main.c
+++ app/src/main.c
@@ -3,32 +3,32 @@
 #include <zephyr/logging/log.h>
 
 #define LED_NODE DT_ALIAS(led0)
+#define BLINK_MS DT_PROP(DT_PATH(zephyr_user), blink_period_ms)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-int main(void)
-{
-    bool led_state = true;
+int main(void) {
+  bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) {
-        return 0;
-    }
-
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
-        return 0;
-    }
-
-    while (true) {
-        if (gpio_pin_toggle_dt(&led) < 0) {
-            return 0;
-        }
-
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(1000);
-    }
-
+  if (!gpio_is_ready_dt(&led)) {
     return 0;
+  }
+
+  if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+    return 0;
+  }
+
+  while (true) {
+    if (gpio_pin_toggle_dt(&led) < 0) {
+      return 0;
+    }
+
+    led_state = !led_state;
+    LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+    k_msleep(BLINK_MS);
+  }
+
+  return 0;
 }
